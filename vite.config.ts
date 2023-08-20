86e3d9a3ae1e86f2d1adc7878841dbc3ec334009
Saharath Kleips <SaharathKleips@gmail.com>
2023-08-19 22:44:19 -0700

    install vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import eslintPlugin from "@nabla/vite-plugin-eslint";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin()],
+  plugins: [
+    react(),
+    eslintPlugin(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   base: "/frame-kit/",
 });
